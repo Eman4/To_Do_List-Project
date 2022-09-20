11e793ad607274148f2ab7d7c3410b8ac5514253
Eman Omar <eman_omar2015@hotmail.com>
2022-09-20 16:00:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { useState } from 'react';
+import AddTaskForm from './components/AddTaskForm';
+import UpdateForm from './components/UpdateForm';
+import ToDo from './components/ToDo';
+import todo from './image/todo.jfif';
+
 
 function App() {
+  const [toDo, setToDo] = useState([]);
+
+  const [newTask, setNewTask] = useState('');
+  const [updateData, setUpdateData] = useState('');
+
+  const addTask = () => {
+    if(newTask){
+      let num = toDo.length + 1;
+      let newEntry = {id : num, title : newTask, status : false}
+      setToDo([...toDo,newEntry])
+      setNewTask('');
+
+    }
+  }
+
+  const deleteTask = (id) => {
+    let newTasks = toDo.filter(task => task.id !== id)
+    setToDo(newTasks);
+  }
+
+  const markDone = (id) => {
+    let newTask = toDo.map(task => {
+      if(task.id === id){
+        return({...task, status: !task.status})
+      }
+      return task;
+    })
+    setToDo(newTask);
+  }
+
+  const cancelUpdate = () => {
+    setUpdateData('');
+  }
+
+  const changeTask = (e) => {
+    let newEntry = {
+      id: updateData.id,
+      title : e.target.value,
+      status : updateData.status ? true : false
+    }
+    setUpdateData(newEntry);
+  }
+
+  const updateTask = () => {
+    let filterRecords = [...toDo].filter(task => task.id !== updateData.id);
+    let updatedObject = [...filterRecords, updateData]
+    setToDo(updatedObject);
+    setUpdateData('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container App">
+      <br/><br/>
+      <img src={todo} alt='todo'/>
+      <br/><br/>
+
+      {/*update task*/}
+      {updateData && updateData ? (
+        <UpdateForm
+        updateData = {updateData}
+        changeTask = {changeTask} 
+        updateTask = {updateTask}
+        cancelUpdate = {cancelUpdate}/>
+      ):(
+        <AddTaskForm 
+        newTask = {newTask}
+        setNewTask = {setNewTask}
+        addTask = {addTask}/>
+      )}
+     
+      {toDo && toDo.length ?'':<h3>No Tasks......</h3>}
+      <ToDo
+      toDo = {toDo}
+      markDone = {markDone}
+      setUpdateData = {setUpdateData}
+      deleteTask = {deleteTask}
+      />
     </div>
   );
 }
